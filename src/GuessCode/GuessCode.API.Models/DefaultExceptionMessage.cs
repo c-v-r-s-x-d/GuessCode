591fe275956ab3508537f9b2aece268b6433f0b9
c-v-r-s-x-d <thienlao683@gmail.com>
2025-05-07 18:43:04 +0400

    + add status code to error message

--- src/GuessCode/GuessCode.API.Models/DefaultExceptionMessage.cs
+++ src/GuessCode/GuessCode.API.Models/DefaultExceptionMessage.cs
@@ -1,7 +1,11 @@
-﻿namespace GuessCode.API.Models;
+﻿using System.Net;
+
+namespace GuessCode.API.Models;
 
 public class DefaultExceptionMessage
 {
+    public HttpStatusCode Status { get; set; }
+    
     public string ErrorMessage { get; set; } = null!;
 
     public string? ErrorSource { get; set; }
--- src/GuessCode/GuessCode.API/Middlewares/ApiExceptionHandler.cs
+++ src/GuessCode/GuessCode.API/Middlewares/ApiExceptionHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
 using GuessCode.API.Models;
 using Microsoft.AspNetCore.Diagnostics;
 
@@ -12,20 +13,21 @@ public class ApiExceptionHandler : IExceptionHandler
         {
             case ValidationException ex:
                 httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
-                await httpContext.Response.WriteAsJsonAsync(BuildErrorResponse(exception), cancellationToken);
+                await httpContext.Response.WriteAsJsonAsync(BuildErrorResponse(exception, HttpStatusCode.BadRequest), cancellationToken);
                 break;
             case not null:
                 httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
-                await httpContext.Response.WriteAsJsonAsync(BuildErrorResponse(exception), cancellationToken);
+                await httpContext.Response.WriteAsJsonAsync(BuildErrorResponse(exception, HttpStatusCode.InternalServerError), cancellationToken);
                 break;
         }
 
         return true;
     }
 
-    private static DefaultExceptionMessage BuildErrorResponse(Exception exception) => new()
+    private static DefaultExceptionMessage BuildErrorResponse(Exception exception, HttpStatusCode statucCode) => new()
     {
         ErrorMessage = exception.Message,
-        ErrorSource = exception.Source
+        ErrorSource = exception.Source,
+        Status = statucCode
     };
 }
